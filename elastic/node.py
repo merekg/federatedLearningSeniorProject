c7405d5c24bea0d94f5a6e62d0ab22d36d7f82b8
merek.goodrich <merekg@gmail.com>
2020-10-07 20:44:53 -0600

    Node private variables, addMatrix function added.

--- elastic/node.py
+++ elastic/node.py
@@ -9,13 +9,9 @@ import threading
 import sys
 # Add the folders above
 sys.path.append('../')
-import node_client.py
+#import node_client.py
 
 class Node:
-
-    # Which machine is this
-    _nodeID = -1
-    _ipAddr = -1
 
     # queue of all things that need to be sent
     _sendingQueue = queue.SimpleQueue()
@@ -28,21 +24,21 @@ class Node:
         ipAddr = f.read()
         
         # Get rid of the periods, cast to int
-        _ipAddr = int(ipAddr.replace('.', ''))
+        self._ipAddr = int(ipAddr.replace('.', ''))
 
         # Determine the node index based on the ip address.
-        if(_ipAddr == 100097):
-            _nodeID = 0; # desktop
-        if(_ipAddr == 1000236):
-            _nodeID = 1;
-        if(_ipAddr == 1000219):
-            _nodeID = 2;
+        if(self._ipAddr == 100097):
+            self._nodeID = 0; # desktop
+        if(self._ipAddr == 1000236):
+            self._nodeID = 1;
+        if(self._ipAddr == 1000219):
+            self._nodeID = 2;
 
         # Threads for sending and receiving information from the other nodes
-        _sendingThread = threading.Thread(target=Node.sendingLoop, daemon=True)
-        _sendingThread.start()
-        _receivingThread = threading.Thread(target=Node.receivingLoop, daemon=True)
-        _receivingThread.start()
+        self._sendingThread = threading.Thread(target=Node.sendingLoop, daemon=True)
+        self._sendingThread.start()
+        self._receivingThread = threading.Thread(target=Node.receivingLoop, daemon=True)
+        self._receivingThread.start()
 
         time.sleep(40)
 
@@ -55,6 +51,22 @@ class Node:
         while(True):
             print("receiving")
             time.sleep(3)
+
+    # This script will take a matrix and divide it into n parts.
+    # It will make the parts as evenly sized as possible while still maintaining the rows and columns.
+    def matrixSplit(matrix, n):
+
+        diff = 0
+        fraction = len(matrix)/n
+        for i in range(0,n):
+            split  = round(fraction + diff)
+            print(split)
+            diff = diff + fraction - split
+            print(diff)
+
+    def addMatrix(self, matrix):
+
+        self._matrix = matrix
 
 
 if (__name__ == "__main__"):
